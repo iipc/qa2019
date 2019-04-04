8ac106df3525330cfd07585adcd2c9147deb7427
James Kafader <jkafader@archive.org>
2019-04-04 16:00:48 +0000

    moved 'sift' to saqa

--- sift/input/local_file.py
+++
@@ -1,5 +0,0 @@
-'''Local File Input Module
-The `fetch` function returns a file handle to a local path.'''
-
-def fetch(url, **kwargs):
-   return open(url, 'r')
--- sift/templates/README.md
+++
@@ -1,3 +0,0 @@
-# Templates Directory
-
-This is the place to put html templates used by the `display.py` flask app
