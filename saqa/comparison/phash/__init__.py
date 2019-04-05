146628aac67544a61437b0ebf143e2a284168b10
Emmanuel Di Pretoro <edipretoro@gmail.com>
2019-04-05 11:59:00 +0000

    Updating the comparison methods to return the path to the screenshots

--- saqa/comparison/phash/__init__.py
+++ saqa/comparison/phash/__init__.py
@@ -3,6 +3,7 @@ Computing the differences between perceptual hashes of the images."""
 
 from PIL import Image
 import imagehash
+import os
 
 
 def compare(original_fh, archived_fh):
@@ -10,4 +11,8 @@ def compare(original_fh, archived_fh):
     original_phash = imagehash.phash(Image.open(original_fh.name), hash_size=hash_size)
     archived_phash = imagehash.phash(Image.open(archived_fh.name), hash_size=hash_size)
 
-    return (1 - (original_phash - archived_phash ) / 2**hash_size, None)
+    return (1 - (original_phash - archived_phash ) / 2**hash_size, {
+        "screenshots_path": os.path.dirname(original_fh.name),
+        "original_screenshot": os.path.basename(original_fh.name),
+        "archived_screenshot": os.path.basename(archived_fh.name)
+    })
