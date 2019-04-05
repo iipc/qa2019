9ace671a49a8d2a43277c358bd84af23e0180d5f
Chase H.D <cdool@loc.gov>
2019-04-05 10:13:54 +0000

    fetch is same as @anjackson but with different network name

--- qa-logs-python/fetch_har.sh
+++ qa-logs-python/fetch_har.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-docker exec -d puppeteer node renderer.js $1
+docker run -i --rm --cap-add=SYS_ADMIN \
+--network qa-logs-python_pywb-puppet --env-file env.render \
+-v $PWD/rendered:/output \
+-v $PWD/puppeteer-har.js:/puppeteer-har.js \
+-v $PWD/renderer.js:/renderer.js \
+--name puppeteer-chrome ukwa/webrender-puppeteer node renderer.js $1
