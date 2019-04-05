e5a0e554e5064aeb28bd6f945b7efdd089bf2f82
Emmanuel Di Pretoro <edipretoro@gmail.com>
2019-04-05 11:58:08 +0000

    Updating the Flask app to display screenshots

--- saqa/display_app.py
+++ saqa/display_app.py
@@ -1,7 +1,10 @@
 from flask import Flask
-from flask import render_template
-app = Flask(__name__)
+from flask import render_template, send_from_directory
 import json
+
+
+app = Flask(__name__)
+app.config["SCREENSHOTS_DIRECTORY"] = '/home/manu/projets/promise/explorations/screenshots_qc'
 
 @app.route("/<result_file>")
 def index(result_file):
@@ -11,3 +14,7 @@ def index(result_file):
             "result_filename": "test.output.json"
             }
     return render_template('index.html', **context)
+
+@app.route('/screenshot/<path:filename>')
+def screenshot_static(filename):
+    return send_from_directory(app.config['SCREENSHOTS_DIRECTORY'], filename)
