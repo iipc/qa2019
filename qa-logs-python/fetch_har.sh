d75904b39cc203742fb4f7c7446ecaac56c9e42d
Chase H.D <cdool@loc.gov>
2019-04-05 11:13:06 +0000

    added check to see if param is file or string

--- qa-logs-python/fetch_har.sh
+++ qa-logs-python/fetch_har.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
-docker run -i --rm --cap-add=SYS_ADMIN \
---network qa-logs-python_pywb-puppet --env-file env.render \
--v $PWD/rendered:/output \
--v $PWD/puppeteer-har.js:/puppeteer-har.js \
--v $PWD/renderer.js:/renderer.js \
---name puppeteer-chrome ukwa/webrender-puppeteer node renderer.js $1
+cmd="docker run -i --rm --cap-add=SYS_ADMIN 
+--network qa-logs-python_pywb-puppet --env-file env.render 
+-v $PWD/rendered:/output 
+-v $PWD/puppeteer-har.js:/puppeteer-har.js 
+-v $PWD/renderer.js:/renderer.js "
+if [[ -f $1 ]]; then
+    cmd="$cmd -v $PWD/$1:/$1 
+    --name puppeteer-chrome ukwa/webrender-puppeteer node renderer.js $1
+    "
+else
+    cmd="$cmd --name puppeteer-chrome ukwa/webrender-puppeteer node renderer.js $1
+    "
+fi
+
+eval $cmd
